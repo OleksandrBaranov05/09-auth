6ffc1092a1944242384345cf361abf5620e23674
Oleksandr Baranov <baranov.sanyok1995@gmail.com>
2025-10-22 01:54:53 +0200

    ProfilePage.module.css

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,39 +1,49 @@
+// app/(private routes)/profile/page.tsx
 import type { Metadata } from 'next';
+import Image from 'next/image';
 import css from './ProfilePage.module.css';
 
 export const metadata: Metadata = {
   title: 'Profile — NoteHub',
-  description: 'Your profile page.',
+  description: 'Your profile page',
   openGraph: {
     title: 'Profile — NoteHub',
-    description: 'Your profile page.',
+    description: 'Your profile page',
     url: 'https://notehub.app/profile',
     images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
   },
 };
 
 export default function ProfilePage() {
+
+  const username = 'your_username';
+  const email = 'your_email@example.com';
+  const avatar = 'https://ac.goit.global/img/no-user.png';
+
   return (
     <main className={css.mainContent}>
       <div className={css.profileCard}>
         <div className={css.header}>
           <h1 className={css.formTitle}>Profile Page</h1>
-          <a href="/profile/edit" className={css.editProfileButton}>Edit Profile</a>
+          <a href="/profile/edit" className={css.editProfileButton}>
+            Edit Profile
+          </a>
         </div>
 
         <div className={css.avatarWrapper}>
-          <img
-            src="https://ac.goit.global/img/no-user.png"
+          <Image
+            src={avatar}
             alt="User Avatar"
             width={120}
             height={120}
             className={css.avatar}
+            priority
           />
         </div>
 
         <div className={css.profileInfo}>
-          <p>Username: your_username</p>
-          <p>Email: your_email@example.com</p>
+          <p>Username: {username}</p>
+          <p>Email: {email}</p>
         </div>
       </div>
     </main>
